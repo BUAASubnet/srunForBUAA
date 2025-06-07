55157ea5a51b2868004de4a16fd6675ad67f3df4
Muheng Li <thelmh@outlook.com>
2025-06-07 11:45:16 +0800

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,11 +1,12 @@
 use std::{env, net::Ipv4Addr};
 
 fn main() {
-    let auth_server_ip = env!(
-        "AUTH_SERVER_IP",
-        "Expect env AUTH_SERVER_IP, export AUTH_SERVER_IP=10.0.0.1"
-    );
-    println!("ENV AUTH_SERVER_IP = 10.200.21.4");
+    // 直接将 IP 地址硬编码，不再从环境变量读取
+    let auth_server_ip = "10.200.21.4";
+
+    println!("Using hardcoded AUTH_SERVER_IP = {}", auth_server_ip);
+    
+    // 解析硬编码的 IP 地址
     auth_server_ip
         .parse::<Ipv4Addr>()
         .expect("AUTH_SERVER_IP invalid");
