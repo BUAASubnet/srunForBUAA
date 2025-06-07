4580569a808f3a7b364bdcc6b5d92a32b83909f6
Muheng Li <thelmh@outlook.com>
2025-06-07 11:41:37 +0800

    Update build.rs

--- build.rs
+++ build.rs
@@ -5,7 +5,7 @@ fn main() {
         "AUTH_SERVER_IP",
         "Expect env AUTH_SERVER_IP, export AUTH_SERVER_IP=10.0.0.1"
     );
-    println!("ENV AUTH_SERVER_IP = {auth_server_ip}");
+    println!("ENV AUTH_SERVER_IP = 10.200.21.4");
     auth_server_ip
         .parse::<Ipv4Addr>()
         .expect("AUTH_SERVER_IP invalid");
